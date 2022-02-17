0eaa3ee8adaf26a2e40ba09606cce89d2306ff29
Leo <aammlj2002@gmail.com>
2022-02-17 08:48:36 +0630

    hello world

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>hello, {{name}}</h1>
+    <h3>In this series, we're going to use following frameworks:</h3>
+    <ul>
+        <li v-for="framework in frameworks" :key="framework" v-text="framework"></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array
+    }
+}
+</script>
+
+<style>
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return inertia('Welcome',[
+        "name"=>"Aung Aung Myo Myat",
+        "frameworks"=>["laravel", "vue", "inertia"]
+    ]);
 });

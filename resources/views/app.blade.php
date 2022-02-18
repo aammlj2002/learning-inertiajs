e4a6778cca356d709dc41e6057ce3e1f2b2c10c4
Leo <aammlj2002@gmail.com>
2022-02-18 09:31:19 +0630

    splitting code by dynamic page import

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Learning Inertia</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/manifest.js') }}" defer></script>
+    <script src="{{ mix('/js/vendor.js') }}" defer></script>
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
 </head>
